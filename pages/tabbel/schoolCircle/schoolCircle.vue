a53bb064b4d169e8996ccc32cde6bb047c779f6d
郭建林 <1050070285@qq.com>
2020-08-03 15:02:36 +0800

    2020-0803 校圈内容

--- pages/tabbel/schoolCircle/schoolCircle.vue
+++ pages/tabbel/schoolCircle/schoolCircle.vue
@@ -1,10 +1,36 @@
 <template>
-	<view>
+	<view class="box">
 		<view class="tabs">
-
+			<v-tabs :tabs="list" @change="activeTab" v-model="chooseTab"></v-tabs>
 		</view>
 		<view class="dynamicList">
+			<view class="tbsBottom"></view>
+			<view :key="index" class="dynamic" v-for="(item,index) in dynamicList">
+				<view class="dynamicNav">
+					<u-avatar :src="item.pic" class="dynamicAvater"></u-avatar>
+					<view class="dynamicName">{{item.name}}</view>
+					<view class="dynamicAddTime">{{item.addTime}}</view>
+				</view>
+				<view class="dynamicSubject">
+					<view class="dynamicContent">{{item.content}}</view>
+					<view class="dynamicMedia">
+<!--						{{src}}-->
+						<view :key="indexImg" class="dynamicImage" v-for="(srcI,indexImg) in  item.imgList" v-if="item.imgList">
+							<image :src="srcI" class="image" v-if="indexImg <=4"></image>
+							<view class="image" v-if="indexImg >4">
+								<image class="more" src="../../../static/images/more.png"></image>
+								查看更多
+							</view>
 
+						</view>
+						<view :key="indexvideo" class="dynamicVideo" v-for="(srcV,indexvideo) in  item.video" v-if="item.video">
+							<video :src="srcV" class="video"></video>
+						</view>
+
+					</view>
+				</view>
+
+			</view>
 		</view>
 	</view>
 </template>
