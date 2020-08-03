de2ed7a3abc536039df28d522416e44b53e24e3b
郭建林 <1050070285@qq.com>
2020-08-03 15:03:06 +0800

    2020-0803 校圈内容

--- pages/tabbel/schoolCircle/schoolCircle.js
+++ pages/tabbel/schoolCircle/schoolCircle.js
@@ -1,90 +1,90 @@
+import api from '../../../utils/request/api'
+import constant from "../../../utils/constant";
 export default {
     data() {
         return {
             list: [
-                {
-                    name: '热门动态'
-            },
-                {
-                    name: '所有动态'
-            },
-                {
-                    name: '所有动态'
-                },
-                {
-                    name: '所有动态'
-                },
-                {
-                    name: '所有动态'
-                },
-                {
-                    name: '所有动态'
-                },
-                {
-                    name: '所有动态'
-                },
-                {
-                    name: '所有动态'
-                },
-                {
-                    name: '所有动态'
-                },
-                {
-                    name: '所有动态'
-                },
-
-
+                '所有动态',
+                '热门动态',
+                '以书会友',
+                '校园爱情',
+                '百团大战',
+                '约起开黑',
+                '操场相见',
+                '个人杂物'
             ],
-            tabs:[
-                {
-                    title:'4545454'
-                },
-                {
-                    title:'4545454'
-                },
-                {
-                    title:'4545454'
-                },
-                {
-                    title:'4545454'
-                }, {
-                    title:'4545454'
-                },
-
-
-            ],
-            // 因为内部的滑动机制限制，请将tabs组件和swiper组件的current用不同变量赋值
-            current: 0, // tabs组件的current值，表示当前活动的tab选项
-            swiperCurrent: 0, // swiper组件的current值，表示当前那个swiper-item是活动的
+            chooseTab:0,
+            dynamicList:[]
         };
     },
-    methods:{
-        /*滑动栏swiper*/
-        // tabs通知swiper切换
-        tabsChange(index) {
-            console.log('tabs通知swiper切换',index)
-            this.current=index
-            this.swiperCurrent = index;
-        },
-        // swiper-item左右移动，通知tabs的滑块跟随移动
-        transition(e) {
-            console.log('swiper-item左右移动，通知tabs的滑块跟随移动',e)
-            let dx = e.detail.dx;
-            this.$refs.uTabs.setDx(dx);
-        },
-        // 由于swiper的内部机制问题，快速切换swiper不会触发dx的连续变化，需要在结束时重置状态
-        // swiper滑动结束，分别设置tabs和swiper的状态
-        animationfinish(e) {
-            console.log(' swiper滑动结束，分别设置tabs和swiper的状态',e)
-            // let current = e.detail.current;
-            let current=this.list.length
-            this.$refs.uTabs.setFinishCurrent(current);
-            // this.swiperCurrent = current;
-            // this.current = current;
-        },
-        // scroll-view到底部加载更多
-        onreachBottom() {
+    onLoad(){
+        /*获取缓存中的个人信息*/
+        this.userInfo = constant.getUserLogin();
+    },
+    onReady(){
+        this.feachData(3)
 
-        }
+    },
+    methods:{
+        /*选项卡*/
+        //选项卡改变时 val`s type Nunmer
+        activeTab(val){
+        //     console.log('选项卡',val)
+        //     switch(val) {
+        //         case 0:
+        //             this.forEachData(3)
+        //             break;
+        //         case 1:
+        //             this.forEachData(3)
+        //             break;
+        //         case 2:
+        //             this.forEachData(3)
+        //             break;
+        //         case 3:
+        //             this.forEachData(3)
+        //             break;
+        //         case 4:
+        //             this.forEachData(3)
+        //             break;
+        //         case 5:
+        //             this.forEachData(3)
+        //             break;
+        //         case 6:
+        //             this.forEachData(3)
+        //             break;
+        //         case 7:
+        //             this.forEachData(3)
+        //             break;
+        //         case 8:
+        //             this.forEachData(3)
+        //             break;
+        //         default:
+        //             // 默认代码块
+        //             console.log('tabs栏炸了')
+        //     }
+        },
+        //获得所有动态
+      async  feachData(e){
+            // console.log(this.userInfo)
+              let res = await api.getDynamicList({
+                  query: {
+                      sign: this.userInfo.sign,
+                      page: 1,
+                      type: e
+                  }
+              })
+
+          this.dynamicList=res.data.dynamicList
+          console.log(this.dynamicList)
+        },
+        //动态进行分类
+        forEachData(e){
+            this.dynamicList.map(val=>{
+                if (val.type==e){
+                    this.dynamicList=[]
+                    this.dynamicList.push(val)
+                }
+            })
+        },
     }
 }
