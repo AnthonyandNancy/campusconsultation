d7cf708c96d59051e84152a96fda4cb8cafe0b6f
郭建林 <1050070285@qq.com>
2020-08-03 17:49:52 +0800

    2020-0803

--- pages/tabbel/schoolCircle/schoolCircle.js
+++ pages/tabbel/schoolCircle/schoolCircle.js
@@ -3,89 +3,17 @@ import constant from "../../../utils/constant";
 export default {
     data() {
         return {
-            list: [
-                '所有动态',
-                '热门动态',
-                '以书会友',
-                '校园爱情',
-                '百团大战',
-                '约起开黑',
-                '操场相见',
-                '个人杂物'
-            ],
-            chooseTab:0,
-            dynamicList:[]
+
         };
     },
     onLoad(){
-        // this.feachData(3)
-        /*获取缓存中的个人信息*/
-        this.userInfo = constant.getUserLogin();
+
     },
     onReady(){
-        this.feachData(3)
 
     },
-    methods:{
-        /*选项卡*/
-        //选项卡改变时 val`s type Nunmer
-        activeTab(val){
-        //     console.log('选项卡',val)
-        //     switch(val) {
-        //         case 0:
-        //             this.forEachData(3)
-        //             break;
-        //         case 1:
-        //             this.forEachData(3)
-        //             break;
-        //         case 2:
-        //             this.forEachData(3)
-        //             break;
-        //         case 3:
-        //             this.forEachData(3)
-        //             break;
-        //         case 4:
-        //             this.forEachData(3)
-        //             break;
-        //         case 5:
-        //             this.forEachData(3)
-        //             break;
-        //         case 6:
-        //             this.forEachData(3)
-        //             break;
-        //         case 7:
-        //             this.forEachData(3)
-        //             break;
-        //         case 8:
-        //             this.forEachData(3)
-        //             break;
-        //         default:
-        //             // 默认代码块
-        //             console.log('tabs栏炸了')
-        //     }
-        },
-        //获得所有动态
-      async  feachData(e){
-            // console.log(this.userInfo)
-              let res = await api.getDynamicList({
-                  query: {
-                      sign: this.userInfo.sign,
-                      page: 1,
-                      type: e
-                  }
-              })
+    onShow(){
 
-          this.dynamicList=res.data.dynamicList
-          console.log(this.dynamicList)
-        },
-        //动态进行分类
-        forEachData(e){
-            this.dynamicList.map(val=>{
-                if (val.type==e){
-                    this.dynamicList=[]
-                    this.dynamicList.push(val)
-                }
-            })
-        },
-    }
+    },
+    methods: {}
 }
--- pages/tabbel/schoolCircle/schoolCircle.vue
+++ pages/tabbel/schoolCircle/schoolCircle.vue
@@ -1,45 +1,7 @@
 <template>
 	<view class="box">
-		<view class="tabs">
-			<v-tabs :tabs="list" @change="activeTab" v-model="chooseTab"></v-tabs>
-		</view>
-		<view class="dynamicList">
-			<view class="tbsBottom"></view>
-			<view :key="index" class="dynamic" v-for="(item,index) in dynamicList">
-				<view class="dynamicNav">
-					<u-avatar :src="item.pic" class="dynamicAvater"></u-avatar>
-					<view class="dynamicName">{{item.name}}</view>
-					<view class="dynamicAddTime">{{item.addTime}}</view>
-				</view>
-				<view class="dynamicSubject">
-					<view class="dynamicContent">{{item.content}}</view>
-					<view class="dynamicMedia">
-<!--						{{src}}-->
-						<view :key="indexImg" class="dynamicImage" v-for="(srcI,indexImg) in  item.imgList" v-if="item.imgList">
-							<image :src="srcI" class="image" v-if="indexImg <=4"></image>
-							<image :src="srcI" class="image" v-if="indexImg <=4"></image>
-							<image :src="srcI" class="image" v-if="indexImg <=4"></image>
-							<image :src="srcI" class="image" v-if="indexImg <=4"></image>
-							<image :src="srcI" class="image" v-if="indexImg <=4"></image>
-							<view class="image" v-if="indexImg >4">
-								<image class="more" src="../../../static/images/more.png"></image>
-								查看更多
-							</view>
 
-						</view>
-<!--						<view :key="indexvideo" class="dynamicVideo" v-for="(srcV,indexvideo) in  item.video" v-if="item.video">-->
-<!--							<video :src="srcV" class="video"></video>-->
-<!--						</view>-->
-
-					</view>
-					<view class="dynamicFooter">
-
-					</view>
-				</view>
-
-			</view>
-		</view>
-	</view>
+<</view>
 </template>
 
 <script src="./schoolCircle.js"></script>
