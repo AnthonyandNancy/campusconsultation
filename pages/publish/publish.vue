f593a98e3606fd3feb9a009b4831ab6f36d9df2b
郭建林 <1050070285@qq.com>
2020-08-04 17:42:12 +0800

    2020-0727 add chatRoom`s Pages

--- pages/publish/publish.vue
+++ pages/publish/publish.vue
@@ -48,6 +48,7 @@
             <v-tabs :tabs="list" @change="activeTab" activeColor="#fff" auto backgroundColor="#007AFF" borderRadius="10rpx" lineHeight="0" v-model="chooseTab"></v-tabs>
         </view>
 
+        
         <view class="operation">
 
             <view class="item">
