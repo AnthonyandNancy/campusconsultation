5f41a538d9eed28c7af65d86431ce00ffb83014c
郭建林 <1050070285@qq.com>
2020-08-05 13:56:04 +0800

    2020-0727 add chatRoom`s Pages

--- pages/publish/publish.vue
+++ pages/publish/publish.vue
@@ -46,7 +46,7 @@
             @群聊
         </view>
         <view class="tab" v-if="chooseActiveTab==true">
-            <view style="width: 800%;">
+            <view style="width:105vh;">
                 <view   @click="activeTab(index)" class="tabss" v-for="(item,index) in list ">{{item.value}}</view>
             </view>
 <!--            <v-tabs :tabs="list" @change="activeTab" activeColor="#fff" auto backgroundColor="#007AFF" borderRadius="10rpx" lineHeight="0" v-model="chooseTab"></v-tabs>-->
