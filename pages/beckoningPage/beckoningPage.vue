869f41d0b641887ed9fd92b413db016bad8b31ed
郭建林 <1050070285@qq.com>
2020-08-06 13:59:09 +0800

    2020-0806

--- pages/beckoningPage/beckoningPage.vue
+++ pages/beckoningPage/beckoningPage.vue
@@ -9,33 +9,33 @@
             <view style="height: 20vh;width: 20vh;border: #AAAAAA solid 1rpx;border-radius: 50%;margin: -59%auto"></view>
 
 
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 20%;left: 6%;border-radius: 50%;" v-if="showOnce ==0"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 6%;left: 43%;border-radius: 50%;" v-if="showOnce ==0"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 12%;left: 58%;border-radius: 50%;" v-if="showOnce ==0"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 25%;left: 75%;border-radius: 50%;" v-if="showOnce ==0"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 39%;left: 54%;border-radius: 50%;" v-if="showOnce ==0"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 18%;left: 5%;border-radius: 50%;" v-if="showOnce ==0"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 8%;left: 20%;border-radius: 50%;" v-if="showOnce ==0"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 50%;left: 25%;border-radius: 50%;" v-if="showOnce ==0"></image>
+            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 31%;left: 6%;border-radius: 50%;" v-if="showOnce ==0"></image>
+            <image src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3137788164,3047565970&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 6%;left: 43%;border-radius: 50%;" v-if="showOnce ==1"></image>
+            <image src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2176278262,3880914969&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 12%;left: 58%;border-radius: 50%;" v-if="showOnce ==0"></image>
+            <image src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1425556110,3594995578&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 25%;left: 75%;border-radius: 50%;" v-if="showOnce ==2"></image>
+            <image src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3715266424,2105950746&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 39%;left: 54%;border-radius: 50%;" v-if="showOnce ==0"></image>
+            <image src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1408754782,2716501159&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 18%;left: 8%;border-radius: 50%;" v-if="showOnce ==2"></image>
+            <image src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3010381143,3139585776&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 20%;left: 20%;border-radius: 50%;" v-if="showOnce ==0"></image>
+            <image src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2972292751,477878368&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 50%;left: 55%;border-radius: 50%;" v-if="showOnce ==1"></image>
 
 
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 20%;left: 6%;border-radius: 50%;" v-if="showOnce ==1"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 6%;left: 43%;border-radius: 50%;" v-if="showOnce ==1"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 12%;left: 58%;border-radius: 50%;" v-if="showOnce ==1"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 25%;left: 75%;border-radius: 50%;" v-if="showOnce ==1"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 39%;left: 54%;border-radius: 50%;" v-if="showOnce ==1"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 18%;left: 5%;border-radius: 50%;" v-if="showOnce ==1"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 8%;left: 20%;border-radius: 50%;" v-if="showOnce ==1"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 50%;left: 25%;border-radius: 50%;" v-if="showOnce ==1"></image>
+            <image src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1491243003,3243338389&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 6%;left: 20%;border-radius: 50%;" v-if="showOnce ==0"></image>
+            <image src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2424276626,1818680797&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 6%;left: 43%;border-radius: 50%;" v-if="showOnce ==1"></image>
+            <image src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2877389213,2599167416&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 39%;left: 27%;border-radius: 50%;" v-if="showOnce ==1"></image>
+            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3845519701,2443907131&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 22%;left: 78%;border-radius: 50%;" v-if="showOnce ==0"></image>
+            <image src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1555549871,3534027392&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 44%;left: 68%;border-radius: 50%;" v-if="showOnce ==1"></image>
+            <image src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2382918433,2255241348&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 8%;left: 46%;border-radius: 50%;" v-if="showOnce ==2"></image>
+            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=152159322,843580480&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 20%;left: 69%;border-radius: 50%;" v-if="showOnce ==1"></image>
+            <image src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2373957739,3101362962&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 4%;left: 50%;border-radius: 50%;" v-if="showOnce ==0"></image>
 
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 20%;left: 6%;border-radius: 50%;" v-if="showOnce ==2"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 6%;left: 43%;border-radius: 50%;" v-if="showOnce ==2"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 12%;left: 58%;border-radius: 50%;" v-if="showOnce ==2"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 25%;left: 75%;border-radius: 50%;" v-if="showOnce ==2"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 39%;left: 54%;border-radius: 50%;" v-if="showOnce ==2"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 18%;left: 5%;border-radius: 50%;" v-if="showOnce ==2"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 8%;left: 20%;border-radius: 50%;" v-if="showOnce ==2"></image>
-            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3882965100,1215744740&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 50%;left: 25%;border-radius: 50%;" v-if="showOnce ==2"></image>
+            <image src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1935305313,1780754080&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 20%;left: 6%;border-radius: 50%;" v-if="showOnce ==2"></image>
+            <image src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=160904937,292575384&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 30%;left: 43%;border-radius: 50%;" v-if="showOnce ==1"></image>
+            <image src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3791410418,1422775378&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 28%;left: 58%;border-radius: 50%;" v-if="showOnce ==0"></image>
+            <image src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2196335857,1963202493&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 38%;left: 75%;border-radius: 50%;" v-if="showOnce ==2"></image>
+            <image src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3307215210,3880575437&fm=11&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 27%;left: 78%;border-radius: 50%;" v-if="showOnce ==1"></image>
+            <image src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1741910632,706818033&fm=11&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 44%;left: 30%;border-radius: 50%;" v-if="showOnce ==0"></image>
+            <image src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2459304394,2737429761&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 20%;left: 66%;border-radius: 50%;" v-if="showOnce ==2"></image>
+            <image src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3118284406,1254551003&fm=26&gp=0.jpg" style="width: 5vh;height: 5vh;position: absolute;top: 45%;left: 25%;border-radius: 50%;" v-if="showOnce ==2"></image>
 
 
 
@@ -83,7 +83,7 @@
                 leftsrc: '../../static/images/peoples.png',
                 Rightsrc: '../../static/images/peoples.png',
                 matchingNum: 10,
-                showOnce:0
+                showOnce:2
             };
         },
         onLoad() {
@@ -91,12 +91,57 @@
                 title: '怦然心动'
             })
             this.setIntervals()
+            this.setImage()
         },
         onUnload() {
             clearInterval(this.time)
             clearInterval(this.matchingTime)
         },
         methods: {
+            /*头像闪烁*/
+            setImage(){
+                //第一次
+                setTimeout(()=>{
+                    this.showOnce=1
+                },1500)
+
+                setTimeout(()=>{
+                    this.showOnce=2
+                },2500)
+
+                setTimeout(()=>{
+                    this.showOnce=0
+                },3500)
+
+                setTimeout(()=>{
+                    this.showOnce=1
+                },4500)
+
+                setTimeout(()=>{
+                    this.showOnce=0
+                },5500)
+
+                setTimeout(()=>{
+                    this.showOnce=1
+                },6500)
+
+                setTimeout(()=>{
+                    this.showOnce=2
+                },7500)
+
+
+                setTimeout(()=>{
+                    this.showOnce=1
+                },8500)
+
+                setTimeout(()=>{
+                    this.showOnce=0
+                },9500)
+
+
+            },
+
+
             setIntervals() {
                 //时间倒计时
                 this.time = setInterval(() => {
@@ -112,7 +157,7 @@
                     this.matchingTimeFun()
                     if (this.setTime <= 0) {
                         // this.showChat=true
-                        // this.showSetTime = true
+                        this.showSetTime = true
                         clearInterval(this.time)
                         clearInterval(this.matchingTime)
                     }
@@ -146,7 +191,7 @@
                         });
                     }, 5000)
                 } else if (this.setTime <= 0) {
-                    // this.showSetTime = true
+                    this.showSetTime = true
                     clearInterval(this.time)
                     clearInterval(this.matchingTime)
                 }
@@ -160,7 +205,7 @@
                 this.setTime = 10
                 this.showSetTime = false
                 this.setIntervals()
-
+                this.setImage()
             },
         }
     }
